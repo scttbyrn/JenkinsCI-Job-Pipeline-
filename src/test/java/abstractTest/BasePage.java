c4afcff40f93bf3dd22ba074dfbfeba7ae4fa18f
Scott <scttsmrfng@gmail.com>
2026-05-01 15:06:22 +0800

    JenkinCI

--- src/test/java/abstractTest/BasePage.java
+++ src/test/java/abstractTest/BasePage.java
@@ -1,9 +1,13 @@
 package abstractTest;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -15,11 +19,30 @@ public class BasePage {
 	public WebDriver driver;
 	public LandingPage landingpage;
 	
-	public WebDriver initializer() {
+	public WebDriver initializer() throws IOException {
 		
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		Properties properties = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//java//resources//GlobalData.properties");
+		properties.load(fis);
 		
+		String browserName = System.getProperty("browser") !=null ? System.getProperty("browser") : properties.getProperty("browser"); //set the browser environment in mvn 
+		
+//		String browserName = properties.getProperty("browser"); //set the browser environment locally
+		
+		//
+		
+		if (browserName.equalsIgnoreCase("chrome")) {
+			
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+	
+		}
+		else if (browserName.equalsIgnoreCase("edge")) {
+			
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
@@ -28,7 +51,7 @@ public class BasePage {
 	}
 	
 	@BeforeMethod (alwaysRun = true)
-	public LandingPage launchBrowser() {
+	public LandingPage launchBrowser() throws IOException {
 		
 		driver = initializer();
 		
